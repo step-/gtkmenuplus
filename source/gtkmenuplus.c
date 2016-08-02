d72e9dff18e1583796e87bb7602bd156d92b1267
step- <step-@users.noreply.github.com>
2016-08-02 15:40:36 +0200

    Hide 'find' error messages.
    Typically due to invalid symlinks, since here 'find' follows.

--- source/gtkmenuplus.c
+++ source/gtkmenuplus.c
@@ -1917,7 +1917,7 @@ off_t createLauncherDB(IN const gchar *rpath, OUT gchar *outf, OUT gchar* sErrMs
  int maxdepth = MAX_SUBMENU_DEPTH - gl_nLauncherReadLineDepth - 1;
  maxdepth = maxdepth > 0 ? maxdepth : 1;
  if (snprintf(cmd, MAX_PATH_LEN,
-       "\"${GTKMENUPLUS_FIND:-find}\" -L '%s' -maxdepth ${GTKMENUPLUS_SCAN_DEPTH:-%d} '(' -type f -o -type l ')' -name '*.desktop' > '%s'",
+       "\"${GTKMENUPLUS_FIND:-find}\" -L '%s' -maxdepth ${GTKMENUPLUS_SCAN_DEPTH:-%d} '(' -type f -o -type l ')' -name '*.desktop' > '%s' 2>&1",
          rpath, maxdepth, outf))
  {
   system(cmd);
