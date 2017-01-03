79c149f2f9579bb505f9af4f609f05b876bc663a
step- <step-@users.noreply.github.com>
2017-01-03 23:12:22 +0100

    Update test library and add test descriptions.

--- test/lib/common.sh
+++ test/lib/common.sh
@@ -14,10 +14,44 @@ export YAD_OPTIONS
 
 case $1 in
   list)
-    find . -perm +111 -type f -name \*.gtkmenuplus |
-    sed 's/..//' |
-    yad --geometry 500x500+60+60 \
-      --list --listen --column="Test file:TEXT" \
+    stat --printf="%n\t%y\t%a\n" *.gtkmenuplus |
+    awk -F '\t' '
+$3 ~ /^[75]/ {
+  filename = $1
+  print filename # OUTPUT
+  sub(/\.[^.]+$/, "\n&", $1)
+  print $1 # NAME & TYPE
+  print substr($2,1,index($2,".")-1) # MODIFIED
+
+  # GIT
+  cmd = "git log --pretty=%cdX%h --date=format:%Y-%m-%d\" \"%H:%M:%S --reverse --abbrev-commit -- " filename
+  cmd | getline gitlog
+  close(cmd)
+  if("" == gitlog) gitlog = "X"
+  sub(/X/, "\n", gitlog)
+  print gitlog # GIT FIRST COMMIT DATE & HASH
+  
+  # DESCRIPTION
+  desc = ""
+  while(0 < (getline s < filename)) {
+    if(s ~ /^#%/) {
+      sub(/^#% */, "", s) # leave <TAB> available for indenting
+      desc = desc (desc ? " " : "") s
+    }
+  }
+  close(filename)
+  gsub(/</, "\\&lt;", desc)
+  print desc # DESCRIPTION
+}' |
+    yad --geometry 800x500+60+60 \
+      --list --listen \
+      --column="OUTPUT:HD" --print-column=1 --separator="" \
+      --column="NAME" \
+      --column="TYPE:HD" \
+      --column="MODIFIED" \
+      --column="FIRST COMMIT" \
+      --column="HASH" \
+      --column "DESCRIPTION:TIP" --tooltip-column=7 \
       --dclick-action="sh -c '>&2 which $GTKMENUPLUS; unset TESTFILE; \"$GTKMENUPLUS\" \"\$0\"'"
     ;;
   tree) # $2-DIR
